2c8e8cb55e648f7e2edd051b879d55a71bb72e2c
ryowuandjanet <ryowudata@gmail.com>
2020-08-16 15:29:08 +0800

    20200816 lesson 16 half

--- core/erp/views.py
+++ core/erp/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
+from  core.erp.models import Category,Product
 
 def myfirstview(request):
   data ={
-    'name': "ryowu"
+    'name': "ryowu",
+    'categories': Category.objects.all()
   }
-  return JsonResponse(data)
+  return render(request,'index.html',data)
+
+def mysecondview(request):
+  data ={
+    'name': "ryowu",
+    'categories': Category.objects.all(),
+    'products': Product.objects.all()
+  }
+  return render(request,'index.html',data)
